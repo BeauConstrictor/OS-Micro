646a15954903845cb8ca71a68aec795d1623d732
Beau Constrictor <dobbob11@duck.com>
2026-05-29 17:14:44 +0100

    added a 'dev_find' routine to the dev library

--- src/dev.asm
+++ src/dev.asm
@@ -25,6 +25,34 @@ get_devtype:
   ld   hl,devtype_xmem
   ret
 
+; find a device with the id in a. sets carry if no device was found
+; leaves the found device selected, and returns in a
+; clobbers: a,b, device
+dev_find:
+  ld   b,a
+  ld   a,0
+  ld   (DEV_SELECT),a
+.loop:
+  call busy_wait
+  ld   a,(DEV_STATUS)
+  and  DEV_ID
+  cp   NODEV
+  jr   z,.nodev
+  cp   b
+  jr   z,.found
+.nodev
+  ld   a,(DEV_SELECT)
+  inc  a
+  ld   (DEV_SELECT),a
+  jr   nz,.loop
+  call busy_wait
+  scf
+  ret
+.found:
+  xor  a
+  ld   a,(DEV_SELECT)
+  ret
+
 devtype_nodev:
   .asciiz "No Device"
 devtype_sectd:
