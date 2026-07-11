fa633f37023c0950a864eec805165e9f1ef76ab9
Beau Constrictor <beauconstrictor@tilde.team>
2026-07-11 15:26:33 +0100

    changed version number to 1.0.0 (i felt like this deserved a major version change, not just going to 0.4.0)

--- src/shell.asm
+++ src/shell.asm
@@ -473,7 +473,7 @@ help_txt:
   .binary "help.txt"
   .byte 0
 welcome_msg:
-  .text   "\e[35mWelcome to OS/M Version 0.4.0!\n"
+  .text   "\e[35mWelcome to OS/M Version 1.0.0!\n"
   .asciiz "\e[90mType 'hlp' for help.\e[0m\n\n"
 pre_prompt:
   .asciiz "\e[36m"
